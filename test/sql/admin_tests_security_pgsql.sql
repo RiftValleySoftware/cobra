de2c0eb39b1b418c67ebed3e46ffb30d318a17e9
Chris M <webmaster@magnaws.com>
2018-05-02 08:21:27 -0400

    Tweaked the security SQL, and updated to the latest BADGER.

--- test/sql/admin_tests_security_pgsql.sql
+++ test/sql/admin_tests_security_pgsql.sql
@@ -16,7 +16,9 @@ CREATE TABLE co_security_nodes (
 INSERT INTO co_security_nodes (login_id, access_class, last_access, read_security_id, write_security_id, object_name, access_class_context, ids) VALUES
 (NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
 ('admin', 'CO_Security_Login', '1970-01-01 00:00:00', 2, 2, 'Default Admin', 'a:1:{s:15:\"hashed_password\";s:4:\"JUNK\";}', NULL),
-('cobra', 'CO_Cobra_Login', '1970-01-01 00:00:00', 3, 3, 'Normal COBRA Login (cobra)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', NULL),
-('krait', 'CO_Cobra_Login', '1970-01-01 00:00:00', 4, 4, 'Normal COBRA Login (krait)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', NULL),
-('asp', 'CO_Login_Manager', '1970-01-01 00:00:00', 5, 5, 'Boss COBRA Manager (asp)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '3,4'),
-('king-cobra', 'CO_Login_Manager', '1970-01-01 00:00:00', 6, 6, 'Boss COBRA Manager (king)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '5');
+('norm', 'CO_Security_Login', '1970-01-01 00:00:00', 3, 3, 'Low-Level 1', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', NULL),
+('bob', 'CO_Security_Login', '1970-01-01 00:00:00', 4, 4, 'Low-Level 2', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', NULL),
+('cobra', 'CO_Cobra_Login', '1970-01-01 00:00:00', 5, 5, 'Normal COBRA Login (cobra)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', NULL),
+('krait', 'CO_Cobra_Login', '1970-01-01 00:00:00', 6, 6, 'Normal COBRA Login (krait)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', NULL),
+('asp', 'CO_Login_Manager', '1970-01-01 00:00:00', 7, 7, 'Boss COBRA Manager (asp)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '3,4,5'),
+('king-cobra', 'CO_Login_Manager', '1970-01-01 00:00:00', 8, 8, 'Boss COBRA Manager (king)', 'a:1:{s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '6');
