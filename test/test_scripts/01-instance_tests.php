7d55d2842b7b7dd29d85701bfeef0507c6cd2c8e
Chris M <webmaster@magnaws.com>
2018-05-06 15:54:16 -0400

    Renamed some files, and started the process of testing COBRA user creation.

--- test/test_scripts/01-instance_tests.php
+++ test/test_scripts/01-instance_tests.php
@@ -15,20 +15,20 @@ require_once(dirname(dirname(__FILE__)).'/functions.php');
     
 // -------------------------------- TEST DISPATCHER ---------------------------------------------
 
-function run_tests() {
-    run_test(1, 'Invalid Valid Big Lizard, but Invalid Direct Instantiation Test', 'We have a valid, manager-user-logged-in CHAMELEON, but try to directly instantiate a COBRA instance, which should fail.', 'king-cobra', NULL, 'CoreysGoryStory');
-    run_test(2, 'Invalid Bad Lizard Test', 'We just create a COBRA instance, give it an invalid CHAMELEON, and make sure it is NOT valid.', NULL, NULL, NULL);
-    run_test(3, 'Invalid Lizard Test', 'We just create a COBRA instance, give it a valid, not-even-attempted-logged-in CHAMELEON, and make sure it is NOT valid.', NULL, NULL, NULL);
-    run_test(4, 'Invalid Errored Lizard Test', 'We just create a COBRA instance, give it a valid, mistake-so-not-logged-in CHAMELEON, and make sure it is NOT valid.', 'norm', NULL, 'OOPSIE');
-    run_test(5, 'Invalid Weed Lizard Test', 'We just create a COBRA instance, give it a valid, non-cobra-level-user-logged-in CHAMELEON, and make sure it is NOT valid.', 'norm', 'CodYOzPtwxb4A', NULL);
-    run_test(6, 'Invalid Garden-Variety Lizard Test', 'We just create a COBRA instance, give it a valid, standard-user-logged-in CHAMELEON, and make sure it is NOT valid.', 'krait', 'CodYOzPtwxb4A', NULL);
-    run_test(7, 'Valid Big Lizard Test', 'We just create a COBRA instance, give it a valid, manager-user-logged-in CHAMELEON, and make sure it is valid.', 'king-cobra', NULL, 'CoreysGoryStory');
-    run_test(8, 'Valid God Lizard Test', 'We just create a COBRA instance, give it a valid, God-logged-in CHAMELEON, and make sure it is valid.', 'admin', NULL, CO_Config::$god_mode_password);
+function instance_run_tests() {
+    instance_run_test(1, 'FAIL -Valid Big Lizard, but Invalid Direct Instantiation Test', 'We have a valid, manager-user-logged-in CHAMELEON, but try to directly instantiate a COBRA instance, which should fail.', 'king-cobra', NULL, 'CoreysGoryStory');
+    instance_run_test(2, 'FAIL -Bad Lizard Test', 'We just create a COBRA instance, give it an invalid CHAMELEON, and make sure it is NOT valid.', NULL, NULL, NULL);
+    instance_run_test(3, 'FAIL -Lizard Test', 'We just create a COBRA instance, give it a valid, not-even-attempted-logged-in CHAMELEON, and make sure it is NOT valid.', NULL, NULL, NULL);
+    instance_run_test(4, 'FAIL -Errored Lizard Test', 'We just create a COBRA instance, give it a valid, mistake-so-not-logged-in CHAMELEON, and make sure it is NOT valid.', 'norm', NULL, 'OOPSIE');
+    instance_run_test(5, 'FAIL -Weed Lizard Test', 'We just create a COBRA instance, give it a valid, non-cobra-level-user-logged-in CHAMELEON, and make sure it is NOT valid.', 'norm', 'CodYOzPtwxb4A', NULL);
+    instance_run_test(6, 'FAIL -Garden-Variety Lizard Test', 'We just create a COBRA instance, give it a valid, standard-user-logged-in CHAMELEON, and make sure it is NOT valid.', 'krait', 'CodYOzPtwxb4A', NULL);
+    instance_run_test(7, 'PASS -Big Lizard Test', 'We just create a COBRA instance, give it a valid, manager-user-logged-in CHAMELEON, and make sure it is valid.', 'king-cobra', NULL, 'CoreysGoryStory');
+    instance_run_test(8, 'PASS -God Lizard Test', 'We just create a COBRA instance, give it a valid, God-logged-in CHAMELEON, and make sure it is valid.', 'admin', NULL, CO_Config::$god_mode_password);
 }
 
 // -------------------------------- TESTS ---------------------------------------------
 
-function admin_test_01($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_01($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
     try {
         $cobra_instance = new CO_Cobra($chameleon_instance);
@@ -37,43 +37,43 @@ function admin_test_01($in_login = NULL, $in_hashed_password = NULL, $in_passwor
     }
 }
 
-function admin_test_02($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_02($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $cobra_instance = make_cobra('BUBBA');
 }
 
-function admin_test_03($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_03($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon();
     $cobra_instance = make_cobra($chameleon_instance);
 }
 
-function admin_test_04($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_04($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
     $cobra_instance = make_cobra($chameleon_instance);
 }
 
-function admin_test_05($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_05($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
     $cobra_instance = make_cobra($chameleon_instance);
 }
 
-function admin_test_06($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_06($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
     $cobra_instance = make_cobra($chameleon_instance);
 }
 
-function admin_test_07($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_07($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
     $cobra_instance = make_cobra($chameleon_instance);
 }
 
-function admin_test_08($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_test_08($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
     $cobra_instance = make_cobra($chameleon_instance);
 }
 
 // -------------------------------- STRUCTURE ---------------------------------------------
 
-function run_test($in_num, $in_title, $in_explain, $in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+function instance_run_test($in_num, $in_title, $in_explain, $in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
     $test_num_string = sprintf("%03d", $in_num);
     echo('<div id="test-'.$test_num_string.'" class="inner_closed">');
         echo('<h3 class="inner_header"><a href="javascript:toggle_inner_state(\'test-'.$test_num_string.'\')">TEST '.$in_num.': '.$in_title.'</a></h3>');
@@ -82,7 +82,7 @@ function run_test($in_num, $in_title, $in_explain, $in_login = NULL, $in_hashed_
                 echo('<p class="explain">'.$in_explain.'</p>');
             echo('</div>');
             $st1 = microtime(TRUE);
-            $function_name = sprintf('admin_test_%02d', $in_num);
+            $function_name = sprintf('instance_test_%02d', $in_num);
             $function_name($in_login, $in_hashed_password, $in_password);
             $fetchTime = sprintf('%01.3f', microtime(TRUE) - $st1);
             echo("<h4>The test took $fetchTime seconds to complete.</h4>");
@@ -91,10 +91,10 @@ function run_test($in_num, $in_title, $in_explain, $in_login = NULL, $in_hashed_
 }
 
 ob_start();
-    prepare_databases('admin_tests');
+    prepare_databases('instance_tests');
     
     echo('<div class="test-wrapper" style="display:table;margin-left:auto;margin-right:auto;text-align:left">');
-        echo('<h1 class="header">ADMIN TESTS</h1>');
+        echo('<h1 class="header">INSTANCE TESTS</h1>');
         echo('<div id="instance-tests" class="closed">');
             echo('<h2 class="header"><a href="javascript:toggle_main_state(\'instance-tests\')">BASIC COBRA INSTANTIATION TESTS</a></h2>');
             echo('<div class="container">');
@@ -102,7 +102,7 @@ ob_start();
             
                 $start = microtime(TRUE);
                 
-                run_tests();
+                instance_run_tests();
                 
                 echo('<h5>The entire set of tests took '. sprintf('%01.3f', microtime(TRUE) - $start) . ' seconds to complete.</h5>');
                 
---
+++ test/test_scripts/02-create_tests.php
@@ -0,0 +1,97 @@
+<?php
+/***************************************************************************************************************************/
+/**
+    COBRA Security Administration Layer
+    
+    © Copyright 2018, Little Green Viper Software Development LLC.
+    
+    This code is proprietary and confidential code, 
+    It is NOT to be reused or combined into any application,
+    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+
+    Little Green Viper Software Development: https://littlegreenviper.com
+*/
+require_once(dirname(dirname(__FILE__)).'/functions.php');
+    
+// -------------------------------- TEST DISPATCHER ---------------------------------------------
+
+function create_run_tests() {
+    create_run_test(9, 'FAIL -Create User From COBRA', 'We log in and instantiate CHAMELEON as a manager, then attempt to create a new user from a login we can\'t see.', 'king-cobra', NULL, 'CoreysGoryStory');
+    create_run_test(10, 'PASS -Create User From COBRA', 'We log in and instantiate CHAMELEON as a manager, then attempt to create a new user from a login we can see.', 'asp', NULL, 'CoreysGoryStory');
+}
+
+// -------------------------------- TESTS ---------------------------------------------
+
+function create_test_09($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+    $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
+    $cobra_instance = make_cobra($chameleon_instance);
+    
+    if (isset($cobra_instance) && ($cobra_instance instanceof CO_Cobra)) {
+        $cobra_user_instance = $cobra_instance->make_user_from_login(5);
+
+        if (isset($cobra_user_instance) && ($cobra_user_instance instanceof CO_Cobra_Login)) {
+            echo("<h2 style=\"color:green;font-weight:bold\">The User instance is valid!</h2>");
+        } else {
+            echo("<h2 style=\"color:red;font-weight:bold\">The User instance is not valid!</h2>");
+            echo('<p style="margin-left:1em;color:red;font-weight:bold">Error: ('.$cobra_instance->error->error_code.') '.$cobra_instance->error->error_name.' ('.$cobra_instance->error->error_description.')</p>');
+        }
+    }
+}
+
+function create_test_10($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+    $chameleon_instance = make_chameleon($in_login, $in_hashed_password, $in_password);
+    $cobra_instance = make_cobra($chameleon_instance);
+    
+    if (isset($cobra_instance) && ($cobra_instance instanceof CO_Cobra)) {
+        $cobra_user_instance = $cobra_instance->make_user_from_login(5);
+
+        if (isset($cobra_user_instance) && ($cobra_user_instance instanceof CO_User_Collection)) {
+            echo("<h2 style=\"color:green;font-weight:bold\">The User instance is valid!</h2>");
+        } else {
+            echo("<h2 style=\"color:red;font-weight:bold\">The User instance is not valid!</h2>");
+            echo('<p style="margin-left:1em;color:red;font-weight:bold">Error: ('.$cobra_instance->error->error_code.') '.$cobra_instance->error->error_name.' ('.$cobra_instance->error->error_description.')</p>');
+        }
+    }
+}
+
+// -------------------------------- STRUCTURE ---------------------------------------------
+
+function create_run_test($in_num, $in_title, $in_explain, $in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+    $test_num_string = sprintf("%03d", $in_num);
+    echo('<div id="test-'.$test_num_string.'" class="inner_closed">');
+        echo('<h3 class="inner_header"><a href="javascript:toggle_inner_state(\'test-'.$test_num_string.'\')">TEST '.$in_num.': '.$in_title.'</a></h3>');
+        echo('<div class="main_div inner_container">');
+            echo('<div class="main_div" style="margin-right:2em">');
+                echo('<p class="explain">'.$in_explain.'</p>');
+            echo('</div>');
+            $st1 = microtime(TRUE);
+            $function_name = sprintf('create_test_%02d', $in_num);
+            $function_name($in_login, $in_hashed_password, $in_password);
+            $fetchTime = sprintf('%01.3f', microtime(TRUE) - $st1);
+            echo("<h4>The test took $fetchTime seconds to complete.</h4>");
+        echo('</div>');
+    echo('</div>');
+}
+
+ob_start();
+    prepare_databases('instance_tests');
+    
+    echo('<div class="test-wrapper" style="display:table;margin-left:auto;margin-right:auto;text-align:left">');
+        echo('<h1 class="header">CREATE USER TESTS</h1>');
+        echo('<div id="create-tests" class="closed">');
+            echo('<h2 class="header"><a href="javascript:toggle_main_state(\'create-tests\')">COBRA USER CREATION TESTS</a></h2>');
+            echo('<div class="container">');
+                echo('<p class="explain"></p>');
+            
+                $start = microtime(TRUE);
+                
+                create_run_tests();
+                
+                echo('<h5>The entire set of tests took '. sprintf('%01.3f', microtime(TRUE) - $start) . ' seconds to complete.</h5>');
+                
+            echo('</div>');
+        echo('</div>');
+    echo('</div>');
+$buffer = ob_get_clean();
+die($buffer);
+?>
