2a7e08f78280564a96af1b5d4524d0456177d6cf
Chris M <webmaster@magnaws.com>
2018-10-29 05:16:50 -0400

    Updated the submodule. Updated the license.

--- test/functions.php
+++ test/functions.php
@@ -3,11 +3,22 @@
 /**
     COBRA Security Administration Layer
     
-    © Copyright 2018, Little Green Viper Software Development LLC.
+    © Copyright 2018, Little Green Viper Software Development LLC/The Great Rift Valley Software Company
     
-    This code is proprietary and confidential code, 
-    It is NOT to be reused or combined into any application,
-    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+    LICENSE:
+    
+    FOR OPEN-SOURCE (COMMERCIAL OR FREE):
+    This code is released as open source under the GNU Plublic License (GPL), Version 3.
+    You may use, modify or republish this code, as long as you do so under the terms of the GPL, which requires that you also
+    publish all modificanions, derivative products and license notices, along with this code.
+    
+    UNDER SPECIAL LICENSE, DIRECTLY FROM LITTLE GREEN VIPER OR THE GREAT RIFT VALLEY SOFTWARE COMPANY:
+    It is NOT to be reused or combined into any application, nor is it to be redistributed, republished or sublicensed,
+    unless done so, specifically WITH SPECIFIC, WRITTEN PERMISSION from Little Green Viper Software Development LLC,
+    or The Great Rift Valley Software Company.
+
+    Little Green Viper Software Development: https://littlegreenviper.com
+    The Great Rift Valley Software Company: https://riftvalleysoftware.com
 
     Little Green Viper Software Development: https://littlegreenviper.com
 */
